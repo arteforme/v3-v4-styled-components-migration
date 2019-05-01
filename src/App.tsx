a98dcc376c29fd4d316b0dd98e72afaca879e662
Junior Monta <junior@MacBook-Pro.local>
2019-05-01 10:09:57 -0700

    Simplifying examples

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,53 @@ import './App.css';
 
 import * as React from 'react';
 
-import Link from './Link';
-import LinkAsButton from './LinkAsButton';
-import LinkWithCustomProps from './LinkWithCustomProps';
+import styled from 'styled-components';
+
+const Link = styled.a`
+  border: solid 1px black;
+  border-radius: 5px;
+  padding: 5px;
+  margin: 10px 5px;
+`;
+
+// Approach #1 - This doesn't work.  None of the button props are known to TS
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+const LinkAsButton = styled(Link).attrs<ButtonProps>({
+  as: 'button',
+  className: 'btn btn-primary'
+})`
+  border: solid 1px red;
+`;
+
+// Approach #2 - This works but it only allows `disaabled` to be specified
+// interface MinimalProps {
+//   as: string;
+//   className: string;
+//   disabled?: boolean;
+// }
+// const LinkAsButton = styled(Link).attrs<{}, MinimalProps>({
+//   as: 'button',
+//   className: 'btn btn-primary'
+// })`
+//   border: solid 1px red;
+// `;
+
+// Approach #3 - This works, but `as` and `className` must be hard coded/repeated
+// in the markup
+// type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+// const LinkAsButton = styled(Link)<ButtonProps>`
+//   border: solid 1px red;
+// `;
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Link className="test-1" data-test="test" href="#">
-          Link
-        </Link>
-        {/* This will not work.  Link doesn't know about any button props */}
-        {/* <Link as="button" disabled={false}>
-          Link as Button with as prop in DOM
-        </Link> */}
-        <LinkAsButton disabled={true}>
-          Link as Button with as prop in attrs
-        </LinkAsButton>
+        <Link>Link</Link>
+        {/* use with approach #1 or #2  */}
+        <LinkAsButton disabled={true}>Link as Button</LinkAsButton>
+        {/* use with approach #3  */}
+        {/* <LinkAsButton as="button" className="btn btn-primary" disabled={true}>Link as Button</LinkAsButton> */}
       </div>
     );
   }
