e0877c6c97ea00fb9ff02f5edfa7701163916a5d
Junior Monta <junior@MacBook-Pro.local>
2019-05-01 10:20:00 -0700

    Creating v3 example

--- src/App.tsx
+++ src/App.tsx
@@ -11,44 +11,19 @@ const Link = styled.a`
   margin: 10px 5px;
 `;
 
-// Approach #1 - This doesn't work.  None of the button props are known to TS
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
-const LinkAsButton = styled(Link).attrs<ButtonProps>({
-  as: 'button',
+const LinkAsButton = Link.withComponent('button').extend.attrs<ButtonProps>({
   className: 'btn btn-primary'
 })`
   border: solid 1px red;
 `;
-
-// Approach #2 - This works but it only allows `disaabled` to be specified
-// interface MinimalProps {
-//   as: string;
-//   className: string;
-//   disabled?: boolean;
-// }
-// const LinkAsButton = styled(Link).attrs<{}, MinimalProps>({
-//   as: 'button',
-//   className: 'btn btn-primary'
-// })`
-//   border: solid 1px red;
-// `;
-
-// Approach #3 - This works, but `as` and `className` must be hard coded/repeated
-// in the markup
-// type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
-// const LinkAsButton = styled(Link)<ButtonProps>`
-//   border: solid 1px red;
-// `;
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Link>Link</Link>
-        {/* use with approach #1 or #2  */}
         <LinkAsButton disabled={true}>Link as Button</LinkAsButton>
-        {/* use with approach #3  */}
-        {/* <LinkAsButton as="button" className="btn btn-primary" disabled={true}>Link as Button</LinkAsButton> */}
       </div>
     );
   }
